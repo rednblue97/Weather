ca992d5bef79f9c95f156f3469f954ca13181dc0
CaptureB <airdavid62@gmail.com>
2024-05-04 07:59:08 -0600

    Finished basic outline of pagewith styling, starting javascript

---
+++ weather.js
@@ -0,0 +1,3 @@
+// https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=weather_code&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&temperature_unit=fahrenheit&wind_speed_unit=mph&precipitation_unit=inch&timeformat=unixtime&timezone=America%2FDenver
+
+export function getWeather(lat,lon, timezone) {}
