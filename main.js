85b85d3ad4d0f8d1febcce0ad53ecd971988eb85
CaptureB <airdavid62@gmail.com>
2024-05-04 19:10:44 -0600

    Began working in main.js, able to display current weather on page using API

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
 import "./style.css"
+import { getWeather } from "./weather"
+
+getWeather(10, 10, Intl.DateTimeFormat().resolvedOptions().timeZone).then(renderWeather).
+catch( e => {
+    console.error(e)
+    alert("Error getting weather.")
+})
+    
+
+function renderWeather( { current, daily, hourly }) {
+    renderCurrentWeather(current)
+    // renderDailyWeather(daily)
+    // renderHourlyWeather(hourly)
+    document.body.classList.remove("blurred")
+}
+
+
+function setValue(selector, value, { parent = document} = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+
+function renderCurrentWeather(current) {
+    setValue("current-temp", current.currentTemp)
+    setValue("current-high", current.highTemp)
+    setValue("current-low", current.lowTemp)
+    setValue("current-fl-high", current.highFeelsLike)
+    setValue("current-fl-low", current.lowFeelsLike)
+    setValue("current-wind", current.windSpeed)
+    setValue("current-precip", current.precip)
+}
